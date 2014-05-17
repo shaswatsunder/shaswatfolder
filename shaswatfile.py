b9f83d7a6bae3eca5257d0c74ab38c992b81ad34
Mitali <shaswatsunder.com>
2014-05-17 12:32:57 +0530

    5th commit

--- shaswatfile.py
+++ shaswatfile.py
@@ -11,6 +11,8 @@ disp(10)
 def samplecalc():
 	print "press 1 for addition."
 	print "press 2 for substraction."
+	print "press 3 for multiplication."
+	print "press 4 for division."
 	choice=input("Enter your choice:")
 	if choice==1:
 		a=input("Enter the 1st number:")
@@ -20,6 +22,15 @@ def samplecalc():
 		a=input("Enter the 1st number:")
 		b=input("Enter the 2nd number:")
 		print "The substration is: ",a-b
+	elif choice==3:
+		a=input("Enter the 1st number:")
+		b=input("Enter the 2nd number:")
+		print "The product is: ",a*b
+	elif choice==4:
+		a=input("Enter the 1st number:")
+		b=input("Enter the 2nd number:")
+		print "The quotient is: ",a/b
+		print "The remainder is: ",a%b
 	else:
 		print "wrong choice"
 samplecalc()
