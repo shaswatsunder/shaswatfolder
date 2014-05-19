4bebf6f60bcc2cfd2fe456d7825d506f2be66579
psachin <iclcoolster@gmail.com>
2014-05-19 12:51:16 +0530

    Merged samplecalc() into a Class 'Calculator'

--- shaswatfile.py
+++ shaswatfile.py
@@ -8,34 +8,38 @@ def disp(x):
 		i=i+1		
 disp(10)
 
-def samplecalc():
-	print "press 1 for addition."
-	print "press 2 for substraction."
-	print "press 3 for multiplication."
-	print "press 4 for division."
-	choice=input("Enter your choice:")
-	if choice==1:
-		a=input("Enter the 1st number:")
-		b=input("Enter the 2nd number:")
-		print "The sum is: ",a+b
-	elif choice==2:
-		a=input("Enter the 1st number:")
-		b=input("Enter the 2nd number:")
-		print "The substration is: ",a-b
-	elif choice==3:
-		a=input("Enter the 1st number:")
-		b=input("Enter the 2nd number:")
-		print "The product is: ",a*b
-	elif choice==4:
-		a=input("Enter the 1st number:")
-		b=input("Enter the 2nd number:")
-		print "The quotient is: ",a/b
-		print "The remainder is: ",a%b
-	else:
-		print "wrong choice"
-samplecalc()
+
+class Calculator(object):
+        """simple calc
+        """
+        
+        def samplecalc(self):
+                print "press 1 for addition."
+                print "press 2 for substraction."
+                print "press 3 for multiplication."
+                print "press 4 for division."
+                choice=input("Enter your choice:")
+                if choice==1:
+                        a=input("Enter the 1st number:")
+                        b=input("Enter the 2nd number:")
+                        print "The sum is: ",a+b
+                elif choice==2:
+                        a=input("Enter the 1st number:")
+                        b=input("Enter the 2nd number:")
+                        print "The substration is: ",a-b
+                elif choice==3:
+                        a=input("Enter the 1st number:")
+                        b=input("Enter the 2nd number:")
+                        print "The product is: ",a*b
+                elif choice==4:
+                        a=input("Enter the 1st number:")
+                        b=input("Enter the 2nd number:")
+                        print "The quotient is: ",a/b
+                        print "The remainder is: ",a%b
+                else:
+                        print "wrong choice"
 
 
-
-
-
+                        
+calc = Calculator()
+calc.samplecalc()
